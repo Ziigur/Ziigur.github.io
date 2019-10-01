b63c062d8c1ec7ab2bac45082e18589f19019a01
Zigur <zigur94@gmail.com>
2019-10-01 14:22:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('cache_v1').then(function(cache) {
       return cache.addAll([
-        '/'
+        '/',
+        '/index.html'
       ]);
     })
   );
